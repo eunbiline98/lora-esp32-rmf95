2645c6733231a1cd06fc26c5290bc885160e7dc9
eunbiline98 <aththaareq.mahesa@gmail.com>
2025-01-25 17:07:35 +0700

    Udpate MQTT Connection Setup

--- Program/esp32_node_sender_lora_rfm96w/src/ETH_Config.h
+++ Program/esp32_node_sender_lora_rfm96w/src/ETH_Config.h
@@ -5,21 +5,21 @@
 #include "EthernetSPI2.h"
 #define led_indi 23
 
-const char *MQTT_SERVER = "192.168.8.2";
-const char *MQTT_STATUS = "paditech/pow/3phase";
+const char *MQTT_SERVER = "xxxxx";
+const char *MQTT_STATUS = "xxxxx";
 
-const char *MQTT_POW_STATE = "paditech/pow/3phase/runn";
+const char *MQTT_POW_STATE = "xxxxx";
 const char *WILL_MESSAGE = "Offline";
 
-const char *MQTT_CLIENT_ID = "POW_3-phase-@#!!@$";
-const char *MQTT_USERNAME = "busol";
-const char *MQTT_PASSWORD = "p4d1n3t";
+const char *MQTT_CLIENT_ID = "xxxxx";
+const char *MQTT_USERNAME = "xxxxx";
+const char *MQTT_PASSWORD = "xxxxx";
 
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
 byte mac[] = {0x02, 0xAB, 0xCD, 0xEF, 0x00, 0x01};
 IPAddress ip(192, 168, 8, 161);
-IPAddress dns(202,6,232,66);
+IPAddress dns(202, 6, 232, 66);
 IPAddress gateway(192, 168, 8, 1);
 IPAddress subnet(255, 255, 255, 0);
 
